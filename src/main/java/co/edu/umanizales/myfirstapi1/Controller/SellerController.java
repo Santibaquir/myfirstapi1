1ba94cc3816029fc25421051e79c3874b4f5224e
santiago tibaquira <santibaquira2007@gmail.com>
2025-03-26 20:09:36 -0500

    agregacion location pereira

--- src/main/java/co/edu/umanizales/myfirstapi1/Controller/SellerController.java
+++ src/main/java/co/edu/umanizales/myfirstapi1/Controller/SellerController.java
@@ -10,15 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/seller")
 public class SellerController {
     @GetMapping
-    public String getSeller() {
-        Location manizales = new Location("17001", "Manizales");
+    public Seller getSeller() {
+        Location Manizales = new Location("17001", "Manizales");
+        Location Pereira = new Location("17002", "Pereira");
 
-        Seller Juan =new Seller("1001", "Juan", "Pérez", 'M', (byte) 30,manizales);
-        Seller Maria =new Seller("1002", "María", "Gómez", 'F', (byte) 28,manizales );
-        Seller Carlos =new Seller("1003", "Carlos", "López", 'M', (byte) 35,manizales );
-        Seller Ana=new Seller("1004", "Ana", "Martínez", 'F', (byte) 26,manizales );
-        Seller Pedro =new Seller("1005", "Pedro", "Ramírez", 'M', (byte) 40,manizales );
+        Seller Juan =new Seller("1001", "Juan", "Pérez", 'M', (byte) 30,Manizales);
+        Seller Maria =new Seller("1002", "María", "Gómez", 'F', (byte) 28,Manizales );
+        Seller Carlos =new Seller("1003", "Carlos", "López", 'M', (byte) 35,Pereira );
+        Seller Ana=new Seller("1004", "Ana", "Martínez", 'F', (byte) 26,Pereira );
+        Seller Pedro =new Seller("1005", "Pedro", "Ramírez", 'M', (byte) 40,Manizales );
 
-        return "" ;
+        return Pedro ;
     }
 }
